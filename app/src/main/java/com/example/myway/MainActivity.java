37d320388f530721ed4014adb5c6f5a8beb945c1
Leejaeyeon28 <yeon8754619@naver.com>
2022-08-13 01:27:05 +0900

    MainActivity 추가 코드 작성 (서울시 임산부 지원사업 URL 연결)

--- app/src/main/java/com/example/myway/MainActivity.java
+++ app/src/main/java/com/example/myway/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -11,18 +12,51 @@ import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity {
 
+    ImageButton QRBtn;
+    ImageButton ChkEmptySeatBtn;
+    ImageButton ChkPositionBtn;
+    ImageButton SupportBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        ImageButton QRBtn = findViewById(R.id.QRBtn);
+        QRBtn = findViewById(R.id.QRBtn);
+        ChkEmptySeatBtn = findViewById(R.id.ChkEmptySeatBtn);
+        ChkPositionBtn = findViewById(R.id.ChkPositionBtn);
+        SupportBtn = findViewById(R.id.SupportBtn);
+
         QRBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(getApplicationContext(), QRScan.class);
                 startActivity(intent);
+            }
+        });
+
+        ChkEmptySeatBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), EmptySeat.class);
+                startActivity(intent);
+            }
+        });
+
+        ChkPositionBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), FacilitiesLocationActiivty.class);
+                startActivity(intent);
+            }
+        });
+
+        SupportBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.seoulmomcare.com/main/main.do"));
+                startActivity(intent);
+
             }
         });
     }

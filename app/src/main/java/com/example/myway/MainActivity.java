16303c167b4ef78d4af46a747e43a6aa7c291ae6
SeonJeong <lkjhmnb72714@gmail.com>
2022-08-12 17:47:47 +0900

    QR바텀시트, 빈 좌석, 설정 xml 완성

--- app/src/main/java/com/example/myway/MainActivity.java
+++ app/src/main/java/com/example/myway/MainActivity.java
@@ -2,13 +2,28 @@ package com.example.myway;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ImageButton QRBtn = findViewById(R.id.QRBtn);
+        QRBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), QRScan.class);
+                startActivity(intent);
+            }
+        });
     }
 }

2f3d84d133797f28f17a86430227d2b0dbe927ed
Ugyeong <dnrud0715@swu.ac.kr>
2022-08-13 18:44:48 +0900

    지도뷰 수정 및 배경색상 화이트로 설정

--- app/src/main/java/com/example/myway/FacilitiesLocationActiivty.java
+++ app/src/main/java/com/example/myway/FacilitiesLocationActiivty.java
@@ -10,5 +10,7 @@ public class FacilitiesLocationActiivty extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_facilities);
+
+        
     }
 }

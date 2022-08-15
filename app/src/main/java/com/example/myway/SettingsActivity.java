5fdbf9837d840e13d86913eec22e52fc46e58dbd
SeonJeong <lkjhmnb72714@gmail.com>
2022-08-15 12:11:48 +0900

    설정 위치정보 이용 약관 다이얼로그 추가

--- app/src/main/java/com/example/myway/SettingsActivity.java
+++ app/src/main/java/com/example/myway/SettingsActivity.java
@@ -1,10 +1,15 @@
 package com.example.myway;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.ismaeldivita.chipnavigation.ChipNavigationBar;
 
@@ -17,10 +22,50 @@ public class SettingsActivity extends AppCompatActivity {
     String name;
     String hospital;
 
+    ImageButton btn_rule_set;
+    ImageButton btn_help_set;
+    ImageButton btn_logout_set;
+    TextView versionText;
+
+    AlertDialog.Builder builder;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
+
+        btn_rule_set = findViewById(R.id.btn_rule_set);
+        btn_help_set = findViewById(R.id.btn_help_set);
+        btn_logout_set = findViewById(R.id.btn_logout_set);
+        versionText = findViewById(R.id.versionText);
+
+        btn_rule_set.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                builder = new AlertDialog.Builder(SettingsActivity.this);
+
+                builder.setTitle("위치기반서비스 이용약관").setMessage(R.string.rule);
+
+                builder.setPositiveButton("OK", new DialogInterface.OnClickListener(){
+                    @Override
+                    public void onClick(DialogInterface dialog, int id)
+                    {
+                        Toast.makeText(getApplicationContext(), "OK Click", Toast.LENGTH_SHORT).show();
+                    }
+                });
+
+                builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener(){
+                    @Override
+                    public void onClick(DialogInterface dialog, int id)
+                    {
+                        Toast.makeText(getApplicationContext(), "Cancel Click", Toast.LENGTH_SHORT).show();
+                    }
+                });
+                AlertDialog alertDialog = builder.create();
+                alertDialog.show();
+            }
+        });
 
         //인텐트 전달받기
         Intent intent = getIntent();

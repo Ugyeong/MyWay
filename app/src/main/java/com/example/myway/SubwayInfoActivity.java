2485513a84e027b01523f4c0afe12d7ee64c1657
Ugyeong <dnrud0715@swu.ac.kr>
2022-08-16 16:30:02 +0900

    지번 변환 및 마커 추가(API 파싱 오류)

--- app/src/main/java/com/example/myway/SubwayInfoActivity.java
+++ app/src/main/java/com/example/myway/SubwayInfoActivity.java
@@ -7,34 +7,49 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.ismaeldivita.chipnavigation.ChipNavigationBar;
+import com.naver.maps.geometry.LatLng;
 import com.naver.maps.map.LocationTrackingMode;
 import com.naver.maps.map.MapView;
 import com.naver.maps.map.NaverMap;
 import com.naver.maps.map.OnMapReadyCallback;
+import com.naver.maps.map.overlay.Marker;
+import com.naver.maps.map.overlay.OverlayImage;
 import com.naver.maps.map.util.FusedLocationSource;
 
 import org.xmlpull.v1.XmlPullParser;
 import org.xmlpull.v1.XmlPullParserException;
 import org.xmlpull.v1.XmlPullParserFactory;
 
+import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.UnsupportedEncodingException;
 import java.lang.reflect.Array;
+import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.net.URLEncoder;
 import java.util.ArrayList;
 
 public class SubwayInfoActivity extends AppCompatActivity implements OnMapReadyCallback {
 
     ChipNavigationBar ChipNavigationBar;
     private MapView mapView;
+    private static NaverMap naverMap;
     private static final int LOCATION_PERMISSION_REQUEST_CODE = 1000;
     private FusedLocationSource locationSource;
 
+    String geoquery;
+    Marker marker = new Marker();
     ImageView submap;
     ImageView refresh;
+
+    TextView subname;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -43,6 +58,8 @@ public class SubwayInfoActivity extends AppCompatActivity implements OnMapReadyC
 
         submap = findViewById(R.id.subinfo_submap);
         refresh = findViewById(R.id.subinfo_refresh);
+
+        subname = findViewById(R.id.subinfo_subname);
 
         ChipNavigationBar = findViewById(R.id.ChipNavigationBar);
         mapView = (MapView) findViewById(R.id.subinfo_map);
@@ -109,12 +126,59 @@ public class SubwayInfoActivity extends AppCompatActivity implements OnMapReadyC
         SubApiData apiData = new SubApiData();
         ArrayList<SubData> dataArr = apiData.getData();
 
+        BufferedReader bufferedReader;
+        StringBuilder stringBuilder = new StringBuilder();
+
         for(SubData data : dataArr){
+            try {
+                geoquery = "https://naveropenapi.apigw.ntruss.com/map-geocode/v2/geocode?query="
+                        + URLEncoder.encode(data.getAdr(),"UTF-8");
+                URL geourl = new URL(geoquery);
+                HttpURLConnection conn = (HttpURLConnection) geourl.openConnection();
+                if(conn!=null){
+                    conn.setConnectTimeout(5000);
+                    conn.setReadTimeout(5000);
+                    conn.setRequestMethod("GET");
+                    conn.setRequestProperty("X-NCP-APIGW-API-KEY-ID\t","13tfim55ik");
+                    conn.setRequestProperty("X-NCP-APIGW-API-KEY","x0xNEmBmdoSN0iO4HLywQuoH2WNydnzZOxVhUG6Y");
+                    conn.setDoInput(true);
 
+                    int responseCode = conn.getResponseCode();
+                    if(responseCode ==200){
+                        bufferedReader = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+                    }else{
+                        bufferedReader = new BufferedReader(new InputStreamReader(conn.getErrorStream()));
+                    }
+
+                    String line = null;
+                    while((line = bufferedReader.readLine())!=null){
+                        stringBuilder.append(line+"\n");
+                    }
+
+                    int indexFirst;
+                    int indexLast;
+
+                    indexFirst = stringBuilder.indexOf("\"x\":\"");
+                    indexLast = stringBuilder.indexOf("\",\"y\":");
+                    String x = stringBuilder.substring(indexFirst+5,indexLast);
+                    //double x = Double.parseDouble(stringBuilder.substring(indexFirst+5,indexLast));
+
+                    indexFirst = stringBuilder.indexOf("\"y\":\"");
+                    indexLast = stringBuilder.indexOf("\",\"distance\":");
+                    double y = Double.parseDouble(stringBuilder.substring(indexFirst+5,indexLast));
+
+                    Toast.makeText(getApplicationContext(), x, Toast.LENGTH_SHORT).show();
+                    //setMarker(marker,x,y,R.drawable.ic_subinfo_marker,0);
+
+                    bufferedReader.close();
+                    conn.disconnect();
+                }
+            } catch (UnsupportedEncodingException | MalformedURLException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
         }
-
-
-
     }
 
     public class SubData{
@@ -137,14 +201,6 @@ public class SubwayInfoActivity extends AppCompatActivity implements OnMapReadyC
             this.adr = adr;
         }
 
-        @Override
-        public String toString() {
-            return "SubData{" +
-                    "name='" + name + '\'' +
-                    ", adr='" + adr + '\'' +
-                    '}';
-        }
-
     }
 
     public class SubApiData{
@@ -163,18 +219,19 @@ public class SubwayInfoActivity extends AppCompatActivity implements OnMapReadyC
 
                         XmlPullParserFactory xmlFactory = XmlPullParserFactory.newInstance();
                         XmlPullParser parser = xmlFactory.newPullParser();
-                        parser.setInput(is,"utf-8");
+                        parser.setInput(new InputStreamReader(is, "UTF-8"));
 
                         boolean sName=false, sAdr=false;
                         String name="", adr="";
 
                         //파싱
-                        while(parser.getEventType() != XmlPullParser.END_DOCUMENT){
-                            int type = parser.getEventType();
+                        int type = parser.getEventType();
+                        while(type != XmlPullParser.END_DOCUMENT){
                             SubData data = new SubData();
 
                             if(type==XmlPullParser.START_TAG){
                                 if(parser.getName().equals("item")){
+                                    subname.setText(parser.getAttributeValue(0));
                                     if(parser.getAttributeValue(0).equals("stinNm")){
                                         sName = true;
                                     }else if(parser.getAttributeValue(0).equals("lonmAdr")){
@@ -197,6 +254,7 @@ public class SubwayInfoActivity extends AppCompatActivity implements OnMapReadyC
                                 data.setName(name);
                                 data.setAdr(adr);
 
+                                subname.setText(adr);
                                 dataArr.add(data);
                             }
                             type = parser.next();
@@ -207,6 +265,8 @@ public class SubwayInfoActivity extends AppCompatActivity implements OnMapReadyC
                         e.printStackTrace();
                     } catch (XmlPullParserException e) {
                         e.printStackTrace();
+                    } catch (IndexOutOfBoundsException e){
+                        e.printStackTrace();
                     }
                 }
             };
@@ -216,12 +276,28 @@ public class SubwayInfoActivity extends AppCompatActivity implements OnMapReadyC
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
-
             return dataArr;
         }
 
     }
 
+    //마커 설정
+    private void setMarker(Marker marker, double lat, double lng, int resourceID, int zIndex)
+    {
+        //원근감 표시
+        marker.setIconPerspectiveEnabled(true);
+        //아이콘 지정
+        marker.setIcon(OverlayImage.fromResource(resourceID));
+        //마커의 투명도
+        marker.setAlpha(0.8f);
+        //마커 위치
+        marker.setPosition(new LatLng(lat, lng));
+        //마커 우선순위
+        marker.setZIndex(zIndex);
+        //마커 표시
+        marker.setMap(naverMap);
+    }
+    
     //지도 설정
     @Override
     public void onRequestPermissionsResult(int requestCode,
